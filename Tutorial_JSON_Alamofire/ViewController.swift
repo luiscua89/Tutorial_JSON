a69338371cf99adcb548ae11c7bdafba967807ad
Luis Cua Catzin <luiscuacatzin@Luiss-Mac.local>
2015-06-23 09:22:31 -0700

    Initial Commit

---
+++ Tutorial_JSON_Alamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorial_JSON_Alamofire
+//
+//  Created by Luis Cua Catzin on 23/06/15.
+//  Copyright (c) 2015 Luis Cua Catzin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
